ae535a88a478f3f05358283b0460d90a15f3923e
Fábio Fernandes <fernandesfabio7070@gmail.com>
2020-02-21 16:58:17 -0300

    Add files via upload

---
+++ bubbleSort.py
@@ -0,0 +1,16 @@
+
+
+def bubble_sort(lista):
+    elementos = len(lista)-1
+    ordenado = False
+    while not ordenado:
+        ordenado = True
+        for i in range(elementos):
+            if lista[i] > lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1],lista[i]
+                ordenado = False        
+                print(lista)
+    return lista
+
+teste = bubble_sort([5, 3, 0, 2,1])
+print(teste)
